7b62b40ed766b1145f98524eb78be0c5ced2d251
Steven Taylor <steven.taylor@me.com>
2018-08-07 17:36:53 -0700

    reverted code

--- api/controllers/light.js
+++ api/controllers/light.js
@@ -5,37 +5,9 @@ module.exports.setLightState = (req, res) => {
     let id = req.swagger.params.id.value;
     let state = req.swagger.params.state.value;
 
-    global.module.getDevice(id)
-
-        .then ( (d) => {
-/*
-            // if desired state is on and light is dimmable and was set, turn on via Dimming1
-            if (state === 'on' && d.type.startsWith('light.dimmable')) {
-                let v = d.current;
-
-                if (v && v.set) {
-
-                    global.module.setLoadLevelTarget(id, 'urn:upnp-org:serviceId:Dimming1', v.set)
-                        .then((status) => {
-                            res.json({data: {status: status}, result: 'ok'});
-                        })
-                        .catch((err) => {
-                            res.status(500).json({code: err.code || 0, message: err.message});
-                        });
-
-                    return;
-                }
-            }
-*/
-            // All other cases fall through to standard on
-            global.module.setTarget(id, 'urn:upnp-org:serviceId:SwitchPower1', state === 'on' ? 1 : 0)
-                .then((status) => {
-                    res.json({data: {status: status}, result: 'ok'});
-                })
-                .catch((err) => {
-                    res.status(500).json({code: err.code || 0, message: err.message});
-                });
-
+    global.module.setTarget(id, 'urn:upnp-org:serviceId:SwitchPower1', state === 'on' ? 1 : 0)
+        .then( (status) => {
+            res.json( { data: { status: status }, result : 'ok' } );
         })
         .catch( (err) => {
             res.status(500).json({code: err.code || 0, message: err.message});
--- vera.js
+++ vera.js
@@ -42,7 +42,6 @@ function vera(config) {
 
     const deviceCache = new NodeCache();
     const statusCache = new NodeCache();
-    const dimmerCache = new NodeCache();
 
     const merge = require('deepmerge');
 
@@ -122,19 +121,6 @@ function vera(config) {
             }
         } );
     }
-
-    this.getDevice = (id) => {
-
-        return new Promise( (fulfill, reject) => {
-            let d = deviceCache.get(id);
-
-            if (d) {
-                d['current'] = statusCache.get(id);
-            }
-
-            fulfill(d);
-        });
-    };
 
     this.getDevices = () => {
 
@@ -449,20 +435,10 @@ function vera(config) {
                                 let update = processStates(device['states']);
 
                                 if (update) {
-
                                     if (current !== undefined) {
                                         let newVal = merge(current, update);
 
                                         if (JSON.stringify(current) !== JSON.stringify(newVal)) {
-
-                                            // We need to cache dimmable set values as Vera ignores them
-                                            /*
-                                            if (d.type.startsWith('light.dimmable')) {
-                                                if (newVal.on) {
-                                                    newVal['set'] = newVal.level;
-                                                }
-                                            }
-                                            */
                                             statusCache.set(device.id, newVal);
                                         }
                                     } else {
